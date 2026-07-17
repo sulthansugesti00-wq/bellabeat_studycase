53bd0c6c66eefb363af6ef8c88ec1498225104f9
sulthansugesti00-wq <sulthansugesti00@gmail.com>
2026-07-17 16:44:56 +0700

    feat: created analysis table for k means

--- bellabeat_notebook.R
+++ bellabeat_notebook.R
@@ -29,12 +29,13 @@ master_act_engage_raw <- c(
   lapply(read_csv) %>% 
   bind_rows()
 
-master_engage_raw <- c(
-  list.files(path="Fitabase Data 3.12.16-4.11.16", pattern = "minuteSleep", full.names = TRUE),
-  list.files(path="Fitabase Data 4.12.16-5.12.16", pattern = "minuteSleep", full.names = TRUE)
-) %>%
-  lapply(read_csv) %>%
-  bind_rows()
+# The code below is not needed cause can't find a way to correlate sleep minute with engagement
+# master_engage_raw <- c(
+  # list.files(path="Fitabase Data 3.12.16-4.11.16", pattern = "minuteSleep", full.names = TRUE),
+# list.files(path="Fitabase Data 4.12.16-5.12.16", pattern = "minuteSleep", full.names = TRUE)
+# ) %>%
+  # lapply(read_csv) %>%
+  # bind_rows()
 
 # cleaning activity and engage----------------------------------------------------------------
 
@@ -84,6 +85,28 @@ master_activity %>% count(id, sort = TRUE)
 n_distinct(master_activity$id)
 
 # cleaning engage ---------------------------------------------------------
-glimpse(master_engage_raw)
+#glimpse(master_engage_raw)
+# Dropping this master table, haven't find a peer reviewed research on wearing device while asleep proves more engagemment therefore not using it.
 
-start cleaning here 1 table
+
+# analysis ----------------------------------------------------------------
+
+#creating analysis table
+master_activity_analysis <- master_activity %>%
+  select(
+    id, date, weekday,
+    total_steps,
+    calories,
+    very_active_minutes,
+    fairly_active_minutes,
+    very_active_distance,
+    moderately_active_distance,
+  )
+
+master_act_engage_analysis <- master_act_engage %>% 
+  select(
+    id, date, time,
+    weekday, total_intensity
+  )
+
+# TODO create k means progress (elbow point,clustering)
